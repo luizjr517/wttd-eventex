70b7627397613b281097530be0882be2783a12f7
Luiz Carlos <luizjr517@gmail.com>
2023-05-26 20:07:11 -0300

    Alinhamento do Test

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,14 +1,14 @@
-from django.test import TestCase
-
-
-class Hometest(TestCase):
-    def setUp(self):
-        self.response = self.client.get('/')
-
-    def test_get(self):
-        """GET / must return status code 200"""
-        self.assertEqual(200, self.response.status_code)
-
-    def test_template(self):
-        """Must use index.html"""
-        self.assertTemplateUsed(self.response, 'index.html')
+# from django.test import TestCase
+# from django.shortcuts import resolve_url as r
+#
+# class Hometest(TestCase):
+#     def setUp(self):
+#         self.response = self.client.get(r('home'))
+#
+#     def test_get(self):
+#         """GET / must return status code 200"""
+#         self.assertEqual(200, self.response.status_code)
+#
+#     def test_template(self):
+#         """Must use index.html"""
+#         self.assertTemplateUsed(self.response, 'index.html')
